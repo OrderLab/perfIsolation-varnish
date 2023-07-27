d7cc1ae4721af9cefdabecbab005965484cf4d57
gongxini <hyigong1@jhu.edu>
2023-07-27 16:26:19 -0600

    nit

--- compile.sh
+++ compile.sh
@@ -4,14 +4,15 @@ mkdir -p dist
 cd 4.0.0
 
 ./autogen.sh
-./configure  --prefix=$(pwd)/../dist
+./configure --prefix=$(pwd)/../dist
 
 make -j4
 if [ $? -ne 0 ]; then
-  echo "Failed to build memcached"
+  echo "Failed to build varnishd"
   exit 1
 fi
-make install
+make install 
 cd ..
 echo "export PSANDBOX_VARNISH_DIR=`pwd`/dist" >> $HOME/.bashrc
 echo "export PATH=`pwd`/dist/bin:$PATH" >> $HOME/.bashrc
+echo "export PATH=`pwd`/dist/sbin:$PATH" >> $HOME/.bashrc
